ccb82f914f37df4e24abd45188986cf3c5cc9c3b
David Jaramillo <djaramil@fau.edu>
2023-06-19 15:33:10 -0400

    Update elastic-search.js

--- elastic-search.js
+++ elastic-search.js
@@ -2,8 +2,8 @@
 
 const { Client } = require('@elastic/elasticsearch')
 const client = new Client({
-  cloud: { id: 'my_elastic:ZWFzdHVzMi5henVyZS5lbGFzdGljLWNsb3VkLmNvbTo0NDMkNGEyODQ2Y2QyMzdmNDU5YmIzMThlNzQ3MTI0NDk3MDQkNWIwYjNhNDM3Y2NlNGM1OTgwMDc5ZjcxNGQzYWQ3YzI=' },
-  auth: { apiKey: 'RmZYZXRZZ0JZNFBKamlPeHNhcXo6UGk2a2VGRFRURk9TSFFyN0JBcW9RQQ==' }
+  cloud: { id: '>my_elastic:my-elastic-cloudid>' },
+  auth: { apiKey: '<elastic-api-key>' }
 })
 
 
